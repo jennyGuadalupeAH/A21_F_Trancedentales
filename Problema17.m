d923292bdc1f54852bd2d2bc0f41a26fc3a92afb
jennyGuadalupeAH <91712665+jennyGuadalupeAH@users.noreply.github.com>
2021-11-25 20:30:03 -0600

    Problema17.m

---
+++ Problema17.m
@@ -0,0 +1,48 @@
+% Octave Script
+% School         : Tecnologico de Estdios Superiores de Jilotepec
+% Date           :20211125
+% Title          :Funciones transcedentes, trigonometricas, logaritmicas y exponenciales
+% Description    :Script realizacion de problemas y haci identificar que funcion es del mismo
+% Author         :Jennifer Guadalupe Angeles Hernandez (Jenny)
+% Matricula      : 202123795
+%Grupo           :3101
+% Usage          :octave> /Users/Hola
+%                :octave>Funcion
+% Notes          :Requiere aplicacion octave, usar su linea de comandos
+%                :https://octaveintro.readthedocs.io/en/latest/index.html
+%Problema        : 17
+
+%Limpiador de variables
+clc
+clear
+
+% Paquete Symbolic
+pkg load symbolic
+syms x
+
+% Resolver funcion
+x=(-10:1:10);
+fx=(e.^9*x+18)
+
+%Plotear funcion 
+plot(x,fx)
+hold on;
+grid on;
+plot([-0,0],[-10,10]);
+plot([-10,10],[-0,0]);
+hold off;
+
+%Identificacion de la funcion
+disp('Ninguna de las anteriores')
+disp('Funcion Inyectiva')
+% Rango
+disp('Rango (-oo,+oo)')
+% Dominio
+disp('Dominio (0,+oo)')
+
+% Titulo de la funcion 
+title('Problema fx=(e.^9*x+18) ')
+% Etiqueta para x
+xlabel('valor x')
+%Etiqueta para y 
+ylabel('valor y')
